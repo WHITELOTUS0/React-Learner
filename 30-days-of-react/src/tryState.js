7907835cfd6e415dea4241a180b7625f88e0949b
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-12-19 11:29:49 +0300

    states

--- 30-days-of-react/src/tryState.js
+++ 30-days-of-react/src/tryState.js
@@ -6,15 +6,30 @@ class Trying extends React.Component {
     // declaring state
     state = {
         count: 0,
+        bColor:"white"
+    }
+
+    addOne=()=>{
+        this.setState({count:this.state.count+1})
+    }
+
+    minusOne = ()=>{
+        this.setState({count:this.state.count-1})
+    }
+
+    changeColor=()=>{
+        this.setState({bColor:this.state.bColor==="white"?"black":"white"})
     }
     render() {
         // accessing the state value
         const count = this.state.count
+        const bColor=this.state.bColor
         return (
-            <div className='App'>
+            <div style={{backgroundColor:bColor}}>
                 <h1>{count} </h1>
-                <button onClick={() => this.setState({ count: this.state.count + 1 })}>Add One</button>
-                <button onClick={() => this.setState({ count: this.state.count - 1 })}>Remove One</button>
+                <button onClick={this.addOne}>+1</button>
+                <button onClick={this.minusOne}>-1</button>
+                <button onClick={this.changeColor}>Change Color</button>
 
             </div>
         )
